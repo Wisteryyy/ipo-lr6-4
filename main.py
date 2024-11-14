e1c82c2d9cc244703e896130dfcaae5d031c599d
Sofia Skakun <sofiaskakun30@gmail.com>
2024-11-14 22:30:45 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-with open("text.txt", "r") as file:
-    l = file.readlines()
+with open("text.txt", "r") as file: # открываем файл в формате чтения
+    l = file.readlines() #считываем и присваиваем l
 
-a = input("Введите строку для поиска: ")
+a = input("Введите строку для поиска: ") # вводим строку для посика
 
-dver = []
-for i in l:
-    if a in i:
-        dver.append(i.strip())
+dver = [] # создаем пустой спислк
+for i in l: # перебераем строки в l
+    if a in i: #если наша строка есть и в l
+        dver.append(i) # то добавляем ее в список
     
-dver.sort(key=len)
+dver.sort(key=len) #сортируем по длине
 
-print(f"Найденные строки: {len(dver)}")
-for i in dver:
-    print(i)
+print(f"Найденные строки: {len(dver)}") #выводится количество
+for i in dver: # и затем выводится каждая строка из списка
+    print(i) #выводится
